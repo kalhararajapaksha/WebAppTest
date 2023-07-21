32670a82a51b8b383d702e0230231c50ac7b6d16
kalhararajapaksha <rmkkrajapaksha@students.nsbm.ac.lk>
2023-07-22 00:35:38 +0530

    new changes added

--- client/login-registration-app/src/components/login/LoginForm.jsx
+++ client/login-registration-app/src/components/login/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { useNavigate } from 'react-router-dom';
@@ -6,14 +6,44 @@ import { useNavigate } from 'react-router-dom';
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [keepLoggedIn, setKeepLoggedIn] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const keepLoggedInCookie = localStorage.getItem('keepLoggedIn');
+    if (keepLoggedInCookie !== null) {
+      setKeepLoggedIn(JSON.parse(keepLoggedInCookie));
+    }
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('keepLoggedIn', JSON.stringify(keepLoggedIn));
+  }, [keepLoggedIn]);
+
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigate('/dashboard');
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
       console.log("success");
-      navigate(`/dashboard`);
+      if (keepLoggedIn) {
+        // Set the "Keep me logged in" cookie or local storage here if desired.
+        console.log("Keep me logged in preference saved.");
+      }
+      navigate('/dashboard');
     } catch (error) {
       console.error(error);
     }
@@ -29,6 +59,10 @@ const LoginForm = () => {
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password:</label>
         <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="keepLoggedIn" checked={keepLoggedIn} onChange={(e) => setKeepLoggedIn(e.target.checked)} />
+        <label className="form-check-label" htmlFor="keepLoggedIn">Keep me logged in</label>
       </div>
       <button type="submit" className="btn btn-primary">Login</button>
     </form>
--- client/login-registration-app/src/components/register/RegisterForm.jsx
+++ client/login-registration-app/src/components/register/RegisterForm.jsx
@@ -5,9 +5,32 @@ const RegisterForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      setErrorMessage('Please enter a valid email address.');
+      return;
+    }
+
+
+    if (password.length < 8) {
+      setErrorMessage('Password must have at least 8 characters.');
+      return;
+    }
+
+  
+    if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match.');
+      return;
+    }
+
+
     try {
       const response = await registerUser({ name, email, password });
       window.location.href = '/';
@@ -21,17 +44,19 @@ const RegisterForm = () => {
     <form onSubmit={handleSubmit} className="my-5">
       <h2 className="mb-4">Register</h2>
 
+      {errorMessage && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
+
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email:</label>
-        <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input  className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
       </div>
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password:</label>
         <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       </div>
       <div className="mb-3">
-        <label htmlFor="conformPassword" className="form-label">Conform Password:</label>
-        <input type="password" className="form-control" id="conformPassword" value={name} onChange={(e) => setName(e.target.value)} required />
+        <label htmlFor="confirmPassword" className="form-label">Confirm Password:</label>
+        <input type="password" className="form-control" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
       </div>
       <button type="submit" className="btn btn-primary">Register</button>
     </form>
